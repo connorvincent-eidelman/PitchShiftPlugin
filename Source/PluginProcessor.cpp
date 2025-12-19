04dee9f5a07c5266a72ae26d6f2e5c4eb9f36610
connorvincent-eidelman <cvincent@brandeis.edu>
2025-12-19 11:42:42 -0500

    12/19 working plugin skeleton

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -8,6 +8,9 @@ PitchShiftPluginAudioProcessor::PitchShiftPluginAudioProcessor()
 {
 }
 
+
+
+
 PitchShiftPluginAudioProcessor::~PitchShiftPluginAudioProcessor() {}
 
 void PitchShiftPluginAudioProcessor::prepareToPlay(double /*sampleRate*/, int /*samplesPerBlock*/) {}
@@ -28,7 +31,78 @@ PitchShiftPluginAudioProcessor::createEditor()
 }
 
 bool PitchShiftPluginAudioProcessor::hasEditor() const { return true; }
+
 const juce::String PitchShiftPluginAudioProcessor::getName() const
 {
     return "Pitch Shift";
+}
+
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new PitchShiftPluginAudioProcessor();
+}
+
+bool PitchShiftPluginAudioProcessor::acceptsMidi() const
+{
+    return false;
+}
+
+bool PitchShiftPluginAudioProcessor::producesMidi() const
+{
+    return false;
+}
+
+bool PitchShiftPluginAudioProcessor::isMidiEffect() const
+{
+    return false;
+}
+
+double PitchShiftPluginAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+//==============================================================================
+int PitchShiftPluginAudioProcessor::getNumPrograms()
+{
+    return 1;
+}
+
+int PitchShiftPluginAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void PitchShiftPluginAudioProcessor::setCurrentProgram (int)
+{
+}
+
+const juce::String PitchShiftPluginAudioProcessor::getProgramName (int)
+{
+    return {};
+}
+
+void PitchShiftPluginAudioProcessor::changeProgramName (int, const juce::String&)
+{
+}
+
+//==============================================================================
+void PitchShiftPluginAudioProcessor::getStateInformation (juce::MemoryBlock&)
+{
+}
+
+void PitchShiftPluginAudioProcessor::setStateInformation (const void*, int)
+{
+}
+
+bool PitchShiftPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+    // Only allow stereo in/out
+    if (layouts.getMainInputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    return true;
 }
